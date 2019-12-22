98097a42ed40b55ff7152bb410eeece151c71820
Seung Yeop Yang <SeungYeop.Yang@gmail.com>
2019-12-22 10:24:34 -0600

    updated

--- ch05/findlink2/main.go
+++ ch05/findlink2/main.go
@@ -2,20 +2,41 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 
 	"golang.org/x/net/html"
 )
 
 func main() {
-	doc, err := html.Parse(os.Stdin)
+	for _, url := range os.Args[1:] {
+		links, err := findLinks(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+			os.Exit(1)
+		}
+		for _, link := range links {
+			fmt.Println(link)
+		}
+	}
+}
+
+func findLinks(url string) ([]string, error) {
+	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
-		os.Exit(1)
+		return nil, err
 	}
-	for _, link := range visit(nil, doc) {
-		fmt.Println(link)
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("getting %s: %s", url, resp.Status)
 	}
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+	return visit(nil, doc), nil
 }
 
 func visit(links []string, n *html.Node) []string {
